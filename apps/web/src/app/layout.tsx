7e9d880e28aa65ddb11d027994b2ba9cf4809607
Cruz1122 <juan.cruz37552@ucaldas.edu.co>
2026-04-04 11:44:01 -0500

    fix(layout): update favicon to a new design
    - Replaced the favicon link from "/favicon.svg" to "/aalie-white.svg" to reflect the new branding.

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/aalie-white.svg" type="image/svg+xml" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
